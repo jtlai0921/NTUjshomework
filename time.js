97e305fa34d6858a3dabbfc8c5454f6185d1a1b1
賴紀廷 <you@example.com>
2021-01-09 05:56:16 +0800

    1

--- time.js
+++ time.js
@@ -5,3 +5,21 @@
      document.getElementById("time").innerHTML = c + "&nbsp" + b;
  }
  setInterval(function() { mytime() }, 1000);
+
+ function startTime() {
+     var today = new Date();
+     var hh = today.getHours();
+     var mm = today.getMinutes();
+     var ss = today.getSeconds();
+     mm = checkTime(mm);
+     ss = checkTime(ss);
+     document.getElementById('clock').innerHTML = hh + ":" + mm + ":" + ss;
+     var timeoutId = setTimeout(startTime, 500);
+ }
+
+ function checkTime(i) {
+     if (i < 10) {
+         i = "0" + i;
+     }
+     return i;
+ }
