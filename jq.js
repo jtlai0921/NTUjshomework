153813469c007d5b36dc01c83689b4364166f469
賴紀廷 <you@example.com>
2021-01-12 04:01:32 +0800

    1

--- jq.js
+++ jq.js
@@ -13,3 +13,11 @@ $(function() {
     alert('訊息開始');
     $('h1').html('開始JavaScript');
 });
+$('#ul1').append('<div class="insObj">我是append加入的</div>');
+$('#ul1').prepend('<div class="insObj">我是prepend加入的</div>');
+$('#ul1').before('<div class="insObj">我是before加入的</div>');
+$('#ul1').after('<div class="insObj">我是before加入的</div>');
+$('<div class="insObj">insertBefore：元素之前加入</div>').insertBefore('#ul2');
+$('<div class="insObj">insertAfter：元素之後加入</div>').insertAfter('#ul2');
+$('<div class="insObj">prependTo：元素之前加入</div>').prependTo('#ul2');
+$('<div class="insObj">appendTo：元素之前加入</div>').appendTo('#ul2');
